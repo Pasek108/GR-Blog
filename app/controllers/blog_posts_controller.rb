418cf300bfb914815a3b8ed9cf92556923acac59
Pasek108 <pasartur108@gmail.com>
2025-09-28 19:46:18 +0200

    init

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,53 @@
+class BlogPostsController < ApplicationController
+  before_action :set_blog_post, except: [ :index, :new, :create ]
+  before_action :authenticate_user!, except: [ :index, :show ]
+
+  def index
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+  end
+
+  def show
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_posts_params)
+
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog_post.update(blog_posts_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def blog_posts_params
+    params.expect(blog_post: [ :title, :content, :published_at ])
+  end
+
+  def set_blog_post
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+end
